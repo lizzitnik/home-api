7329ae491c954aa8788b218a59b110b70070a184
Liz Zitnik <lizbiz@Lizs-MacBook-Pro.local>
2018-06-05 11:04:59 -0700

    major fixes

---
+++ routers/quoteRouter.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const axios = require("axios")
+const router = express.Router()
+
+
+router.get('/quote', (req, res, next) => {
+  const QUOTES_URL = 'https://random-quote-generator.herokuapp.com/api/quotes/random'
+  axios(`${QUOTES_URL}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(data => {
+    console.log(data.data)
+    res.send(JSON.stringify(data.data))
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({
+      message: err
+    })
+  })
+})
+
+module.exports = router
